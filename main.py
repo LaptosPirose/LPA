908c0c6b38edc8f91fd5186a9507253a7905c6f1
Angelo Moura <angelomoura@gmail.com>
2024-07-10 18:40:43 -0300

    teste

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import numpy as np
 from time import time
 
 
+class Dog:
+    def __init__(self, name, age) -> None:
+        self.__name = name
+        self.__age = age
+
+    def bark(self):
+        return f'{self.name} is barking!'
+
+    def get_name(self):
+        return self.__name
+
 def main():
-    pass
+    rex = Dog('Rex', 10)
+    print(rex.get_name())
+    print(rex.__class__.__name__)
+    print(Dog.__name__)
 
 if __name__ == '__main__':
     main()
