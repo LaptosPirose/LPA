e18e6cd9dba0eb84509bc7df232d142160fda2bc
Angelo Moura <angelomoura@gmail.com>
2024-07-15 19:25:30 -0300

    imagem Mário

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-
 root = tk.Tk()
 root.title('Python Development')
 root.geometry('800x450+300+100')
@@ -11,6 +10,5 @@ imagem = Image.open('./images/mario.webp').resize((600, 450))
 photo = ImageTk.PhotoImage(imagem)
 
 label = ttk.Label(root, image=photo).pack()
-
 
 root.mainloop()
