d5524c96f48b3b61e64a75867035f4f81f34f0ed
Angelo Moura <angelomoura@gmail.com>
2024-07-15 22:33:40 -0300

    note

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title('Python Development')
-root.geometry('800x450+300+100')
+root.geometry('800x500+300+100')
 
 imagem = Image.open('./images/mario.webp').resize((600, 450))
 photo = ImageTk.PhotoImage(imagem)
 
 label = ttk.Label(root, image=photo).pack()
+
+button = ttk.Button(root, text='Quit', command=root.destroy)
+button.pack()
 
 # Start minloop
 root.mainloop()
